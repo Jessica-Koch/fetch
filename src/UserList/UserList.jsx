8b1f9bbef09fb8efa35954e22e9cda1ef9ae161e
Jessica-Koch <jessicakoch136@icloud.com>
2017-12-15 19:04:14 -0800

    Merge branch 'master' of github.com:Jessica-Koch/fetch

--- src/UserList/UserList.jsx
+++ src/UserList/UserList.jsx
@@ -1,19 +1,51 @@
-import React, {Component} from 'react';
+import { Switch, Route } from 'react-router-dom';
+import React from 'react';
+import './UserList.scss';
+import Users from '../Users';
+import User from '../User';
 
-class UserList extends Component {
-  render() {
-    const users = this.props.users.map(user => <User key={user._links.self.href} user={user}/>);
-
-    return (
-      <div class="grid">
-        <div class="row heading">
-          <div class="header">First Name</div>
-          <div class="header">Last Name</div>
-          <div class="header">Description</div>
-          <div class="header">Pet</div>
-        </div>
-        {users}
-      </div>
-    )
-  }
-}
+const UserList = () => (
+  <div className="section">
+    <h1>This is a User page!</h1>
+    <Switch>
+      <Route exact path="/users" component={Users} />
+      <Route path="users/:number" component={User} />
+    </Switch>
+  </div>
+);
+// class UserList extends Component {
+//   static propTypes = {
+//     users: array,
+//   };
+//
+//   constructor(props) {
+//     super(props);
+//     this.state = { users: this.props.users };
+//   }
+//
+//   componentDidMount() {
+//     fetch('/api/users')
+//       .then(response => response.json())
+//       .then(users => this.setState({ users }));
+//   }
+//
+//   render() {
+//     const users = this.state.users.map(user => <User key={user.id} user={user} />);
+//
+//     return (
+//       <div className="grid-container">
+//         <div className="header">First Name</div>
+//         <div className="header">Last Name</div>
+//         <div className="header">email</div>
+//         <div className="header">Pet</div>
+//
+//         {users}
+//       </div>
+//       <Switch>
+//         <Route exact path='/users' component={UserList}/>
+//       </Switch>
+//     );
+//   }
+// }
+//
+// export default UserList;
