15022c5542f91bcdd2febf6eb2412fb6c951c4f1
Jessica-Koch <jessicakoch136@icloud.com>
2017-12-15 19:04:39 -0800

    add header component that will be the nav

---
+++ components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React, { PureComponent } from 'react';
+import logo from './fox-sitting.png';
+
+class Header extends PureComponent {
+  render() {
+    return (
+      <div className="nav">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to Fetch</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default Header;
