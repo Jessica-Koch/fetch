2ae25e19f99268db845f05007ccbaf3611c59251
Jessica-Koch <jessicakoch136@icloud.com>
2018-10-22 12:44:23 -0700

    configure routes

---
+++ server/src/config/route-config.js
@@ -0,0 +1,7 @@
+// Route initialization
+module.exports = {
+    init(app) {
+        const staticRoutes = require('../routes/static')
+        app.use(staticRoutes)
+    }
+}
--- server/src/server.js
+++ server/src/server.js
@@ -2,7 +2,7 @@ const app = require('./app');
 const http = require('http')
 const server = http.createServer(app);
 
-server.listen(3000);
+server.listen(5000);
 server.on('listening', () => {
-    console.log('server listening for requests on port 3000')
+    console.log('server listening for requests on port 5000')
 })
